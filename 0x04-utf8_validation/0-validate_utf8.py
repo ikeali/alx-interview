8d8dd615de16078cf5362b5211d5769167ef6165
ikeali <ikechukwualisigwe01@gmail.com>
2024-11-01 07:11:25 +0000

    all

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,12 +4,10 @@
 UTF-8 validation module
 """
 
-
 def validUTF8(data):
     """
     Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
-    
     """
     skip = 0
     n = len(data)
